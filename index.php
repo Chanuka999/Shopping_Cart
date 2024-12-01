36ad0d596b88c69c0166c2e27c0299d856632d97
Chanuka999 <chanukaranditha99@gmail.com>
2024-12-01 20:28:14 +0530

    connect to database

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php include "connect.php";  ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,11 +15,11 @@
     <div class="container">
         <section>
             <h3 class="heading">Add Products</h3>
-            <form action="" class="add-product">
-               <input type="text" placeholder="Enter product name" class="input_fields" required>
-               <input type="number" min="0" placeholder="Enter product price" class="input_fields" required>
-               <input type="file"  class="input_fields" required>
-               <input type="submit"  class="submit_btn" value="add product">
+            <form action="" class="add-product" method="POST" enctype="multipart/form-data">
+               <input type="text" name="product_name" placeholder="Enter product name" class="input_fields" required>
+               <input type="number" name="product_price" min="0" placeholder="Enter product price" class="input_fields" required>
+               <input type="file" name="product_image"  class="input_fields" required>
+               <input type="submit" name="add_product"  class="submit_btn" value="add product">
             </form>
         </section>
     </div>
