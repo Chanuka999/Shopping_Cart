fb34fcf01785fbf983f189a67fadf0268c34f84f
Chanuka999 <chanukaranditha99@gmail.com>
2024-12-02 12:22:21 +0530

    create new file

---
+++ view_products.php
@@ -0,0 +1,28 @@
+<?php include "connect.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+<body>
+    <?php include "header.php"; ?>
+    <div class="header">
+        <section class="display_products">
+            <table>
+                <thead>
+                    <th>Sl No</th>
+                    <th>Product Image</th>
+                    <th>Product Name</th>
+                    <th>Product Price</th>
+                    <th>Action</th>
+                </thead>
+            </table>
+        </section>
+    </div>
+</body>
+</html>
