5b372b907708c85a41b2ddf290194fe85f4b2361
Chanuka999 <chanukaranditha99@gmail.com>
2024-12-02 12:21:51 +0530

    update

--- header.php
+++ header.php
@@ -2,8 +2,8 @@
         <div class="header_body">
             <a href="index.php" class="logo">SHOPPING</a>
             <nav class="navbar">
-                <a href="">Add Products</a>
-                <a href="">view Products</a>
+                <a href="index.php">Add Products</a>
+                <a href="view_products.php">view Products</a>
                 <a href="">Shoppit</a>
             </nav>
             <a href="" class="cart"><i class="fa-solid fa-cart-shopping"></i><span><sup>4</sup></span></a>
