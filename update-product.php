bc20ab670607970c7e275966ba97110b9a1c9564
Chanuka999 <chanukaranditha99@gmail.com>
2024-12-03 19:36:31 +0530

    add to select query

--- update-product.php
+++ update-product.php
@@ -1,3 +1,15 @@
+<?php include "connect.php"; 
+
+if(isset($_POST["submit"])){
+  $update_id = $_POST["update_id"];
+  $update_name = $_POST["update_name"];
+  $update_price = $_POST["update_price"];
+  $update_image = $_FILES["update_image"]["name"];
+  $updateImage_tmp_name= $_FILES["update_image"]["tmp_name"];
+  $updateImage_folder = "images/".$update_image;
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,17 +24,42 @@
     <?php include "header.php"; ?>
 
     <section class="edit_container">
-      <form action="" class="update-product product_container_box">
-        <img src="" alt="">
-        <input type="hidden">
-        <input type="text" class="input_fields fields" required>
-        <input type="number" class="input_fields fields" required>
-        <input type="file" class="input_fields fields" required>
+      <?php
+        
+       if(isset($_GET["edit"])){
+        $edit_id = $_GET["edit"];
+
+        $sql = "SELECT * FROM products WHERE id = $edit_id";
+
+        $res = mysqli_query($conn,$sql);
+        $count = mysqli_num_rows($res);
+
+        if($count>0){
+           $row=mysqli_fetch_assoc($res);
+            ?>
+      <form action="" method="POST" enctype="multipart/form-data" class="update-product product_container_box">
+        <img src="images/<?php echo $row['image']; ?>" alt="">
+        <input type="hidden" value="<?php echo $row['id']; ?>" name="update_id">
+        <input type="text" class="input_fields fields" name="update_name" value="<?php echo $row['name']; ?>" required>
+        <input type="number" class="input_fields fields" name="update_price" value="<?php echo $row['price']; ?>" required>
+        <input type="file" class="input_fields fields" name="update_image" required>
         <div class="btns">
-          <input type="submit" class="edit_btn">
+          <input type="submit" class="edit_btn" name="submit">
           <input type="reset" id="close-edit" value="Cancel" class="cancel_btn">
         </div>
       </form>
+
+          <?php
+           
+        }else{
+          echo "not a data in database";
+        }
+        
+       }
+
+    
+      ?>
+      
     </section>
 
 </body>
