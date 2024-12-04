4d032cafde2ebba1b6e66154e498e456e0427c86
Chanuka999 <chanukaranditha99@gmail.com>
2024-12-04 10:56:18 +0530

    create shop product page

---
+++ shop_products.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+<body>
+    <?php include "header.php"; ?>
+    <div class="container">
+        <section class="products">
+            <h1 class="Lets Shop">
+                <div class="product_container">
+                    <div class="edit_form">
+                        <img src="images/img3.jpg" alt="">
+                        <h3>laptop</h3>
+                        <div class="price">Price:200000</div>
+                        <input type="submit" class="submit_btn cart_btn" value="add to cart">
+                    </div>
+                </div>
+            </h1>
+        </section>
+    </div>
+</body>
+</html>
