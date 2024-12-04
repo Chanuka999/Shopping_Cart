87f34cc7d25f87decfa11ac63082fea40634ff15
Chanuka999 <chanukaranditha99@gmail.com>
2024-12-04 11:44:07 +0530

    connect to datbase

--- shop_products.php
+++ shop_products.php
@@ -1,3 +1,13 @@
+<?php
+include "connect.php";
+if(isset($_POST["add_to_cart"])){
+    $product_name = $_POST["product_name"];
+    $product_price =$_POST["product_price"];
+    $product_image = $_POST["product_image"];
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,12 +24,17 @@
         <section class="products">
             <h1 class="Lets Shop">
                 <div class="product_container">
+                    <form method="POST" action="">
                     <div class="edit_form">
                         <img src="images/img3.jpg" alt="">
                         <h3>laptop</h3>
                         <div class="price">Price:200000</div>
+                        <input type="hidden" name="product_name">
+                        <input type="hidden" name="product_price">
+                        <input type="hidden" name="product_image">
                         <input type="submit" class="submit_btn cart_btn" value="add to cart">
                     </div>
+                  </form>
                 </div>
             </h1>
         </section>
