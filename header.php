4db1b907b2a64d6deff0eb2949acb5550ed9d1db
Chanuka999 <chanukaranditha99@gmail.com>
2024-12-05 10:08:10 +0530

    connect to cart page

--- header.php
+++ header.php
@@ -6,7 +6,15 @@
                 <a href="view_products.php">view Products</a>
                 <a href="shop_products.php">Shoppit</a>
             </nav>
-            <a href="" class="cart"><i class="fa-solid fa-cart-shopping"></i><span><sup>4</sup></span></a>
+            <?php
+              $sql= "SELECT * FROM cart";
+
+              $res= mysqli_query($conn,$sql);
+
+              $row=mysqli_num_rows($res);
+
+            ?>
+            <a href="cart.php" class="cart"><i class="fa-solid fa-cart-shopping"></i><span><sup><?php echo $row;  ?></sup></span></a>
             <div id="menu-btn" class="fas fa-bars"></div>
         </div>
     
