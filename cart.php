0bd7222461f5d9cd8cde2f567c2cb3c6f0b2ff4b
Chanuka999 <chanukaranditha99@gmail.com>
2024-12-05 11:31:20 +0530

    create new file

--- cart.php
+++ cart.php
@@ -14,6 +14,48 @@
     <div class="container">
         <section class="shopping_cart">
             <h1 class="heading">MY Cart</h1>
+            <table>
+                <thead>
+                    <th>Sl No</th>
+                    <th>Product Name</th>
+                    <th>product Image</th>
+                    <th>product Price</th>
+                    <th>product Quantity</th>
+                    <th>Total price</th>
+                    <th>Action</th>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>laptop</td>
+                    <td>
+                        <img src="images/images.jpg" alt="/">
+                    </td>
+                    <td>23000</td>
+                    <td>
+                        <div class="quantity_box">
+                            <input type="number" min="1">
+                            <input type="submit" class="update_quantity" value="update">
+                        </div>
+                     </td>
+                    <td>23000</td>
+                    <td>
+                        <a href="">
+                            <i class="fas fa-trash"></i>Remove
+                        </a>
+                    </td>
+                  </tr>
+                </tbody>
+           </table>
+           <div class="table_bottom">
+            <a href="hop_products.php" class="bottom_btn">Continue Shoppng</a>
+            <h3 class="bottom_btn">Grand total:<span>25000/-</span></h3>
+            <a href="checkout.php" class="bottom_btn">Procced to checkout></a>
+           </div>
+
+           <a href="" class="delete_all_btn">
+            <i class="fas fa-trash"></i>Delete All
+           </a>
         </section>
     </div>
 </body>
